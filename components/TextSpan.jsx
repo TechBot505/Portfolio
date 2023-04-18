2ee134357ab1797df5df28d1bb6f02839c026f11
Rohit <rohitdhanotia2003@gmail.com>
2023-04-19 03:11:13 +0530

    bugs bugs

--- components/TextSpan.jsx
+++ components/TextSpan.jsx
@@ -2,19 +2,17 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 
 const TextSpan = ({ children }) => {
-  const rubberBand = () => {
-    return {
-      color: '#9846fc',
-      transform: [
-        "scale3d(1, 1, 1)",
-        "scale3d(1.4, 0.55, 1)",
-        "scale3d(0.75, 1.25, 1)",
-        "scale3d(1.25, 0.85, 1)",
-        "scale3d(0.9, 1.05, 1)",
-        "scale3d(1, 1, 1)"
-      ]
-    };
-  };
+  const rubberBand = () => ({
+    color: '#9846fc',
+    transform: [
+      'scale3d(1, 1, 1)',
+      'scale3d(1.4, 0.55, 1)',
+      'scale3d(0.75, 1.25, 1)',
+      'scale3d(1.25, 0.85, 1)',
+      'scale3d(0.9, 1.05, 1)',
+      'scale3d(1, 1, 1)',
+    ],
+  });
 
   return (
     <motion.span
@@ -23,7 +21,7 @@ const TextSpan = ({ children }) => {
     >
       {children}
     </motion.span>
-  )
+  );
 };
 
 export default TextSpan;
