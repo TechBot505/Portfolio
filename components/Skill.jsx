f73505bca9ddabf2aad62d3cc53912a52e554c56
Rohit <rohitdhanotia2003@gmail.com>
2023-04-19 03:00:11 +0530

    indentation

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -5,19 +5,17 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { slideIn2 } from '../utils/motion';
 
-const Skill = ({ link }) => {
-  return (
-      <div className="group relative flex cursor-pointer">
-          <motion.img
-              variants={slideIn2('left', 'spring', 0.25, 0.8)}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: false, amount: 0.25 }}
-              src={link}
-              className="rounded-full border border-white object-cover w-24 h-24 group-hover:opacity-50 transition duration-300 ease-in-out"
-          />
-      </div>
-  )
-};
+const Skill = ({ link }) => (
+  <div className="group relative flex cursor-pointer">
+    <motion.img
+      variants={slideIn2('left', 'spring', 0.25, 0.8)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      src={link}
+      className="rounded-full border border-white object-cover w-24 h-24 group-hover:opacity-50 transition duration-300 ease-in-out"
+    />
+  </div>
+);
 
 export default Skill;
